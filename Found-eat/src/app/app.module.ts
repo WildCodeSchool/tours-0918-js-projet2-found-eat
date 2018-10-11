735d137494e09494c3bfc27da50e32d92df5dfb5
jonathan Begot <jonat.begot@laposte.net>
2018-10-11 09:55:02 +0200

    supprajout

--- Found-eat/src/app/app.module.ts
+++ Found-eat/src/app/app.module.ts
@@ -1,4 +1,7 @@
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AjoutComponent } from './ajout/ajout.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -8,28 +11,29 @@ import { ModalComponent } from './modal/modal.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
-          
+
 const appRoutes: Routes = [
-{ path: '', redirectTo: '1', pathMatch: 'full' },
-{ path: ':id', component: GalleryComponent },
+  { path: '', redirectTo: '1', pathMatch: 'full' },
+  { path: ':id', component: GalleryComponent },
 ];
 
 @NgModule({
-	declarations: [
-	AppComponent,
-	GalleryComponent,
-	ModalComponent,
-  CarouselComponent,
-  NavBarComponent,
-  FooterComponent,
-	],
-	imports: [
-	BrowserModule,
-	NgbModule,
-	FormsModule,
-	RouterModule.forRoot(appRoutes),
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    ModalComponent,
+    CarouselComponent,
+    NavBarComponent,
+    FooterComponent,
+    AjoutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

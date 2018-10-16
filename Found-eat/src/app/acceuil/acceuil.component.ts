79c3e9ceb0376f3f4edca18fad54cb6b572042a6
Spac3G0at <victor.s.marie@gmail.com>
2018-10-16 09:31:38 +0200

    modif comparateur

--- Found-eat/src/app/acceuil/acceuil.component.ts
+++ Found-eat/src/app/acceuil/acceuil.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-acceuil',
-  templateUrl: './acceuil.component.html',
-  styleUrls: ['./acceuil.component.css']
+	selector: 'app-acceuil',
+	templateUrl: './acceuil.component.html',
+	styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent implements OnInit {
 
-  constructor() { }
+	products: any[];
 
-  ngOnInit() {
-  }
+	constructor(private service: NgbModal) { }
+
+	ngOnInit() {
+		window.scrollTo(0, 0);
+		this.products = JSON.parse(localStorage['products']);
+		this.products = this.products.slice(0, 4);
+	}
+
+	openMedia(content: any) {
+		this.service.open(content).result
+
+		.then(resultat => {
+			console.log(resultat);
+		})
+
+		.catch(error => {
+			console.log(error);
+		});
+	}
+
+
 
 }

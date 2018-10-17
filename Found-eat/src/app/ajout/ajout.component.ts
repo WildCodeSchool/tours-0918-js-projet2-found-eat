598d769e68e9e9564b847def4caa6f87f4ca383a
jonathan Begot <jonat.begot@laposte.net>
2018-10-17 17:24:05 +0200

    fini la reciproque vue produit et formulaire

--- Found-eat/src/app/ajout/ajout.component.ts
+++ Found-eat/src/app/ajout/ajout.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit, } from '@angular/core';
   styleUrls: ['./ajout.component.css']
 })
 export class AjoutComponent implements OnInit {
+
   val: string;
   ene: string;
   lip: string;
   ac: string;
+  glu: string;
+  sug: string;
+  fa: string;
+  pr: string;
+  se: string;
+  so: string;
+  kil: string;
+  inge: string;
+  nom: string;
+
+
+
   constructor() { }
 
   ngOnInit() {

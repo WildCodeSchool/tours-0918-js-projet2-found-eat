d48c8b95bba2cf7622ec12764d903285a70ebcde
bncordier <mrbcordier@gmail.com>
2018-11-06 16:36:37 +0100

    modal sauvegarde pour modification du carousel

--- Found-eat/src/app/slideshow-update/slideshow-update.component.ts
+++ Found-eat/src/app/slideshow-update/slideshow-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SlideimageService } from '../common/slideimage.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-slideshow-update',
@@ -10,14 +11,18 @@ export class SlideshowUpdateComponent implements OnInit {
 
   images: any[];
 
-  constructor(private service: SlideimageService) { }
+  constructor(private service: SlideimageService, private modalService: NgbModal) { }
 
   ngOnInit() {
-  this.images = this.service.images;
+    this.images = this.service.images;
   }
 
   update() {
     this.service.update();
   }
-  
+
+  // open of modal //
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
+  }
 }
